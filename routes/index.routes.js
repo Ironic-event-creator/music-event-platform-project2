7d043644001590e103d6dcce979e9946cc1ce1ae
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-10-13 11:35:40 +0200

    add route to keep alive

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
-
+const Event = require('../models/Event.model')
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+
+
+router.get('/keep-alive', (req, res, next) => {
+  Event.find()
+    .then(() => {
+      res.status(200).json({ message: 'Thanks for keeping our project alive!🫶' });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "It didn't work" });
+    });
 });
 
 module.exports = router;
